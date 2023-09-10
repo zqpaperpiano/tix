f1013bcde11f35a498fb5733e174c1169524930d
zqpaperpiano <ziqing.ong.2021@socsc.smu.edu.sg>
2023-09-11 01:13:06 +0800

    linked pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarCompProfile from './Components/Navbar/NavbarCompProfile';
 import SignUp from './SignUp/SignUp';
@@ -8,21 +9,67 @@ import NavbarComp from './Components/Navbar/NavbarComp';
 import EventsMusic from './Events/EventsMusic';
 import EventsSports from './Events/EventsSports';
 
+class App extends Component{
+  constructor(){
+    super();
+    this.state=({
+      route: 'AllEvents',
+      user: {
+        email: '',
+      }
+    })
+  }
 
+  loadUser = (email) => {
+    this.setState({
+      user: {
+        email: email,
+      }
+    })
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <NavbarComp/>
-      </header>
-      
-      <main>
-        <EventsMusic/>
-      </main>
+  onUserLogin(){
+    if(this.state.user.email != ''){
+      return <NavbarCompProfile onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
+    }else{
+      return <NavbarComp onRouteChange={this.onRouteChange} />
+    }
+  }
 
-    </div>
-  );
+  onRouteChange = (route) => {
+    this.setState({
+      route: route,
+    })
+  }
+
+  pageNavigation(){
+    switch(this.state.route){
+          case 'Login':
+            return <Login onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
+          case 'SignUp':
+            return <SignUp onRouteChange={this.onRouteChange} loadUser={this.loadUser}/>
+          case 'AllEvents':
+            return <EventsAll onRouteChange={this.onRouteChange} />
+          case 'Music':
+            return <EventsMusic onRouteChange={this.onRouteChange} />  
+          case 'Sports':
+            return <EventsSports onRouteChange={this.onRouteChange} />
+        }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header>
+          {this.onUserLogin()}
+        </header>
+        
+        <main>
+          {this.pageNavigation()}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Events/EventsMusic.jsx
+++ src/Events/EventsMusic.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import "./EventsMusic.css";
 import { Button } from 'react-bootstrap';
 
-export const EventsMusic = () => {
+export const EventsMusic = ({onRouteChange}) => {
   return (
     <div className="events-all">
       <div className="div">
 
-        <Button variant="light" className="allbutton" href="#EventsAll">All</Button>
+        <Button onClick={() => onRouteChange('AllEvents')} variant="light" className="allbutton" href="#EventsAll">All</Button>
         <Button variant="light" className="musicbutton" href="#EventsMusic">Music</Button>
-        <Button variant="light" className="sportbutton" href="#EventsSports">Sports</Button>
+        <Button onClick={() => onRouteChange('Sports')} variant="light" className="sportbutton" href="#EventsSports">Sports</Button>
         
         <div className="taylor-swift">
           <p className="text-wrapper">TAYLOR SWIFT | THE ERAS TOUR</p>
