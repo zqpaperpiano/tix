ef9294a9711e15a95bb91e59e2eb109a076dd660
peeniti <peenitis.2022@scis.smu.edu.sg>
2023-09-15 16:24:03 +0800

    event serv

--- src/main/java/com/example/demo/Event/EventController.java
+++ src/main/java/com/example/demo/Event/EventController.java
@@ -5,12 +5,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 @RequestMapping
 public class EventController {
+    @Autowired
+    private EventService eventService;
     @GetMapping
-    public ResponseEntity<String> allEvents() {
-        return new ResponseEntity<String>("All Events", HttpStatus.OK);
+    public ResponseEntity<String> getAllEvents() {
+        return new ResponseEntity<List<Event>>(eventService.allEvents(), HttpStatus.OK);
     }
 }
