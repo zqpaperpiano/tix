775f00dd74b6c45ca824df4a181a0891484c9cf6
peeniti <peenitis.2022@scis.smu.edu.sg>
2023-09-15 15:44:09 +0800

    event

---
+++ src/main/java/com/example/demo/Event/Event.java
@@ -0,0 +1,13 @@
+package com.example.demo.Ticket;
+
+import org.bson.types.ObjectId;
+//import org.springframework.data.mongodb.core.mapping.Document;
+
+
+public class Event {
+    private ObjectId id;
+
+    private String name;
+
+    private String date;
+}
